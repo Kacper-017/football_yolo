cd19057fb5851c49fe0f11efcbafb1c948af3af6
Kacper <kacper.masny2000@gmail.com>
2024-05-20 20:34:03 +0200

    yea, I am going to commit this file

---
+++ yolo_infrenece.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+from torch.cuda import is_available
+
+device = 'cuda' if is_available() else 'cpu'
+print(f"Using {device} device")
+
+model = YOLO('models/best.pt').to(device)
+
+results = model.predict('input_videos/08fd33_4.mp4', save = True)
+
+print(results[0])
+print("===================")
+for box in results[0].boxes:
+    print(box)
+
